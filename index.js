743cc0962d345dfc4b7a07e00ab5ffe0922ce213
Houth Panha Mongkul <101343790+UtaW1@users.noreply.github.com>
2022-12-22 20:47:32 +0700

    Finished

---
+++ index.js
@@ -0,0 +1,11 @@
+const primaryNav = document.querySelector('.primary-nav'); 
+const navToggle = document.querySelector('.mobile-nav-toggle'); 
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible'); 
+    if (visibility === "false") {
+        primaryNav.setAttribute('data-visible', true); 
+    } else {
+        primaryNav.setAttribute('data-visible', false); 
+    }
+})
